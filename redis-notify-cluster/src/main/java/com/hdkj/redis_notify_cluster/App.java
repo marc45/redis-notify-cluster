74cd2a8e89eaf91d31cd4eef5644dc3a56b591ed
xiey <xiey_pacino@iCloud.com>
2017-11-09 13:43:40 +0800

    起始

---
+++ redis-notify-cluster/src/main/java/com/hdkj/redis_notify_cluster/App.java
@@ -0,0 +1,13 @@
+package com.hdkj.redis_notify_cluster;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
