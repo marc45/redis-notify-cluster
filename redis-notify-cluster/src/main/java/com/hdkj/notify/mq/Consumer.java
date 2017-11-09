5ef4ba9252576842331f7796ebdf7c04098d41e8
xiey <xiey_pacino@iCloud.com>
2017-11-09 16:03:24 +0800

    成功集成activeMQ

---
+++ redis-notify-cluster/src/main/java/com/hdkj/notify/mq/Consumer.java
@@ -0,0 +1,18 @@
+package com.hdkj.notify.mq;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author xiey
+ * @version 2017年11月9日 下午3:52:38
+ * @describe
+ */
+@Component
+public class Consumer {
+	// 使用JmsListener配置消费者监听的队列，其中text是接收到的消息
+	@JmsListener(destination = "mytest.queue")
+	public void receiveQueue(String text) {
+		System.out.println("Consumer收到的报文为:" + text);
+	}
+}
